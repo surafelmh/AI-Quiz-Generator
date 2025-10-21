eb47f6824fa1c0e48acab8dc866b6fd450934008
surafelmh <surafelmuluneh@gmail.com>
2025-10-21 11:20:28 -0400

    Implemented login security system

--- backend/models/users.py
+++ backend/models/users.py
@@ -3,5 +3,26 @@ from pydantic import BaseModel, EmailStr
 
 class UserCreate(BaseModel):
     username: str
+    full_name: str | None = None
     email: EmailStr
     password: str
+
+class UserDisplay(BaseModel):
+    username: str
+    full_name: str | None = None
+    email: EmailStr
+    disabled: bool = False
+
+class UserInDB(BaseModel):
+    username: str
+    full_name: str | None = None
+    email: EmailStr
+    hashed_password: str
+    disabled: bool = False
+
+class Token(BaseModel):
+    access_token: str
+    token_type : str
+
+class TokenData(BaseModel):
+    username: str | None = None
